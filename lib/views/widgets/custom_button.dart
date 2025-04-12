852a6728f7106a7269bdf2b664e2b5447c2f5aac
gmulonga <gadsonmulonga@gmail.com>
2025-04-12 10:32:53 +0300

    feat: add a custom button

---
+++ lib/views/widgets/custom_button.dart
@@ -0,0 +1,36 @@
+import 'package:afyaai/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatefulWidget {
+  CustomButton({required this.callBackFunction, required this.label});
+
+  final VoidCallback callBackFunction;
+  final String label;
+
+  @override
+  _CustomButtonState createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        widget.callBackFunction();
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: kGreen,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 17),
+        child: Text(
+          widget.label,
+          style: TextStyle(color: kWhite, fontSize: 15),
+        ),
+      ),
+    );
+  }
+}
