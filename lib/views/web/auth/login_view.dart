ad4772ec55cc7e2d442c30670224212d2d342dd0
gmulonga <gadsonmulonga@gmail.com>
2025-04-13 07:01:47 +0300

    feat: add a spinner to login page

--- lib/views/web/auth/login_view.dart
+++ lib/views/web/auth/login_view.dart
@@ -1,10 +1,10 @@
 import 'package:afyaai/controllers/login_controller.dart';
 import 'package:afyaai/utils/constants.dart';
 import 'package:afyaai/views/widgets/custom_button.dart';
+import 'package:afyaai/views/widgets/custom_spinner.dart';
 import 'package:afyaai/views/widgets/mobile/buttons/custom_button_two.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:afyaai/models/login_model.dart';
 import 'package:afyaai/views/widgets/custom_input.dart';
 
 class LoginWebView extends StatefulWidget {
@@ -27,62 +27,112 @@ class _LoginWebViewState extends State<LoginWebView> {
     }
   }
 
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset('images/afyaAI-logo.png', height: 200),
+          SizedBox(height: 10),
+          Text(
+            'Welcome back',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold, color: kGreen),
+          ),
+          SizedBox(height: 20),
+          InputField(
+            hintText: "example@gmail.com",
+            label: "Email",
+            isRequired: true,
+            controller: emailController,
+          ),
+          InputField(
+            hintText: "********",
+            label: "Password",
+            controller: passwordController,
+            password: true,
+          ),
+          SizedBox(height: 24),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5),
+            child: CustomButton(callBackFunction: _login, label: "Sign In"),
+          ),
+          SizedBox(height: 30),
+          Text("Don't have an account?",
+              textAlign: TextAlign.center, style: TextStyle(color: kGreen)),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+            child: CustomButtonTwo(callBackFunction: () {}, label: "Sign Up"),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (_, constraints) {
+        bool isSmallScreen = constraints.maxWidth < 800;
+
         return Scaffold(
-          body: Center(
-            child: Container(
-              padding: const EdgeInsets.all(10.0),
-              width: constraints.maxWidth < 600 ? constraints.maxWidth * 0.9 : 500,
-              decoration: BoxDecoration(
-                color: kWhite,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 8,
-                    offset: Offset(0, 4),
+          body: Obx(() => Stack(
+            children: [
+              Center(
+                child: isSmallScreen
+                    ? Container(
+                  padding: const EdgeInsets.all(16.0),
+                  width: constraints.maxWidth * 0.9,
+                  decoration: BoxDecoration(
+                    color: kWhite,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 8,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  child: _buildForm(),
+                )
+                    : Row(
                   children: [
-                    Image(
-                      image: AssetImage('images/afyaAI-logo.png'),
-                      height: 250,
-                    ),
-                    Text(
-                      'Welcome back',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: kGreen
+                    Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: kGreen,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          padding: EdgeInsets.all(30),
+                        ),
                       ),
                     ),
-                    SizedBox(height: 10),
-                    InputField(hintText: "example@gmail.com", label: "Email", isRequired: true, controller: emailController,),
-                    InputField(hintText: "********", label: "Password", controller: passwordController, password: true,),
-                    SizedBox(height: 24),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 5),
-                        child: CustomButton(callBackFunction: _login, label: "Sign In")),
-                    SizedBox(height: 30,),
-                    Text("Don't have an account?", textAlign: TextAlign.center, style: TextStyle(color: kGreen),),
-
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-                        child: CustomButtonTwo(callBackFunction: () {}, label: "Sign Up"))
+                    Expanded(
+                      flex: 2,
+                      child: Center(
+                        child: Container(
+                          width: constraints.maxWidth * 0.4,
+                          padding: const EdgeInsets.all(16.0),
+                          child: _buildForm(),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
-          ),
+              if (controller.isLoading.value)
+                Container(
+                  color: Colors.black.withOpacity(0.5),
+                  child: Center(child: spinkit), // make sure `spinkit` is a valid widget
+                ),
+            ],
+          )),
         );
       },
     );
